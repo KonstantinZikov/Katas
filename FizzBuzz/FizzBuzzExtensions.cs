07481a29a1582011c443523d9b1ece5982096cdf
Konstantin Zikov <Konstantin Zikov>
2016-04-01 00:20:48 +0300

    Done

---
+++ FizzBuzz/FizzBuzzExtensions.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzBuzz
+{
+    public static class FizzBuzzExtensions
+    {
+        //elegant
+        public static string FizzBuzz1(this int n)
+            => (n % 5 == 0 ? "Fizz" : "") + (n % 3 == 0 ? "Buzz" : (n % 5 == 0 ? "" : "" + n));
+
+        //fast & readable
+        public static string FizzBuzz2(this int n)
+        {
+            if (n % 5 == 0)
+            {
+                if (n % 3 == 0)
+                    return "FizzBuzz";
+                return "Fizz";
+            }
+            if (n % 3 == 0)
+                return "Buzz";
+            return n.ToString();
+        }
+    }
+}
---
+++ FizzBuzz/FizzBuzzTests.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FizzBuzz
+{
+    [TestClass]
+    public class FizzBuzzTests
+    {
+        [TestMethod]
+        public void One()
+        {
+            Assert.AreEqual("1", 1.FizzBuzz1());
+            Assert.AreEqual("1", 1.FizzBuzz2());
+        }
+
+        [TestMethod]
+        public void Two()
+        {
+            Assert.AreEqual("2", 2.FizzBuzz1());
+            Assert.AreEqual("2", 2.FizzBuzz2());
+        }
+
+        [TestMethod]
+        public void Three()
+        {
+            Assert.AreEqual("Buzz", 3.FizzBuzz1());
+            Assert.AreEqual("Buzz", 3.FizzBuzz2());
+        }
+
+        [TestMethod]
+        public void Five()
+        {
+            Assert.AreEqual("Fizz", 5.FizzBuzz1());
+            Assert.AreEqual("Fizz", 5.FizzBuzz2());
+        }
+
+        [TestMethod]
+        public void Fifteen()
+        {
+            Assert.AreEqual("FizzBuzz", 15.FizzBuzz1());
+            Assert.AreEqual("FizzBuzz", 15.FizzBuzz2());
+        }
+
+        [TestMethod]
+        public void Zero()
+        {
+            Assert.AreEqual("FizzBuzz", 0.FizzBuzz1());
+            Assert.AreEqual("FizzBuzz", 0.FizzBuzz2());
+        }
+
+
+    }
+}
